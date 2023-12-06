c097ae1551f2a6337e9daca58065f77bb2bc9d99
Alan Jason Lewandowski <lewandowski301301@gmail.com>
2023-12-06 17:44:21 -0300

    Blogs Added [Example]

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {motion}  from 'framer-motion'
+import '../styles/App.css'
+function ButtonMotion(){
+    return(
+  <div  >
+ 
+
+  </div>
+
+    )
+}
+
+
+export default ButtonMotion;
